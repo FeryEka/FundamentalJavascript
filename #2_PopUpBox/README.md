bd8155edcb52456bc1173ec2bb6491c6ca67f447
FeryEka <Ferrh.mahendra@gmail.com>
2024-02-08 23:45:19 +0700

    implementation POPUP Box JS

---
+++ #2_PopUpBox/README.md
@@ -0,0 +1,7 @@
+#Results
+-this output prompt
+![alt text](image.png)
+-this output confirm
+![alt text](image-1.png)
+-this output alert
+![alt text](image-2.png)
---
+++ #2_PopUpBox/script.js
@@ -0,0 +1,12 @@
+// POPUP BOX
+// Have 3 sintaks it is alert, prompt, confirm
+// this how to use all sintaks popup box
+
+var nama = prompt('Masukkan Nama : ');
+var display = confirm('Tampilkan Nama?');
+
+if (display == true) {
+    alert(nama);
+} else {
+    alert('anonim');
+}
